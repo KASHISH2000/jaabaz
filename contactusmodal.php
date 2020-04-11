c2cf746d76fbf448550d034e0c847611d87e1807
rishabhkeshan <rishabhkeshan@gmail.com>
2020-04-12 00:38:20 +0530

    updated details of new leads, rectified the contact us form

--- contactusmodal.php
+++ contactusmodal.php
@@ -1,4 +1,3 @@
-
 <div class="modal-content" id="contactus">
 	<div class="modal-headerr" style="padding: 10px;">
 		<br>
@@ -19,8 +18,8 @@
 					<h2 class="text-center underline-head">Technical Department
 						<span class="underline"></span>
 					</h2>
-					<h1 class="text-center" style="color:#dc092f;">Govind Karwa<br><span class="contactus-position">(Team Captain)</span></h1>
-					<h2 class="text-center person-box-phone">+91 8390728275</h2>
+					<h1 class="text-center" style="color:#dc092f;">Satwik Darivemula<br><span class="contactus-position">(Team Captain)</span></h1>
+					<h2 class="text-center person-box-phone">+91 9948071246</h2>
 					<!-- <h1 class="text-center" style="color:#dc092f;">Som Dwivedi<br>(<span class="contactus-position">Team Co-Captain</span>)</h1>
 					<h2 class="text-center person-box-phone">+91 9486476740</h2> -->
 				</div>
@@ -28,8 +27,8 @@
 					<h2 class="text-center underline-head">Marketing Department
 						<span class="underline"></span>
 					</h2>
-					<h1 class="text-center" style="color:#dc092f;">Karan Waghela<br><span class="contactus-position">(Team Manager)</span></h1>
-					<h2 class="text-center person-box-phone">+91 9790666304</h2>
+					<h1 class="text-center" style="color:#dc092f;">Shadhir Haja<br><span class="contactus-position">(Team Manager)</span></h1>
+					<h2 class="text-center person-box-phone">+91 9003730044</h2>
 				</div>
 				<!-- <div class="contactus-person-box col-md-3 col-xs-12">
 					<h2 class="text-center underline-head">Join Jaabaz family
@@ -51,32 +50,32 @@
 							<a class="embed-map-code" rel="nofollow" href="http://www.szablonypremium.pl/website" id="make-map-information">motywy html</a>
 						</div>
 						<script src="https://www.szablonypremium.pl/google-maps-authorization.js?id=65d4e24c-8508-5507-f23c-d5aaad35bc87&c=embed-map-code&u=1475047957" defer="defer" async="async"></script>
-						
+
 					</section>
 				</div>
 				<div class="col-md-4">
 					<div class="form-container">
-					<h3 class="text-center"><a href="mailto:official@teamjaabaz.com">teamjaabazracing@gmail.com</a></h3>
+						<h3 class="text-center"><a href="mailto:teamjaabaz.vit@gmail.com">teamjaabaz.vit@gmail.com</a></h3>
 						<h2 style="color:#dc092f;" class="text-center">We’d love to hear from you</h2>
-						
-							<div class="text-field-name">
-								<input type="text" title="name" required autocomplete="off" name="name" id="name" placeholder="Name*" onfocus="this.placeholder = '';" onblur="if (this.placeholder == '') {this.placeholder = 'Name*';}"> 
-							</div>
-							<div class="text-field-name">
-								<input type="email" title="Your Email" required autocomplete="off" name="email" id="email" placeholder="Email*" onfocus="this.placeholder = '';" onblur="if (this.placeholder == '') {this.placeholder = 'Email*';}">
-							</div>
-							<div class="text-field-subject">
-								<input type="text" title="Subject" required autocomplete="off" name="subject" id="subject" placeholder="Subject*" onfocus="this.placeholder = '';" onblur="if (this.placeholder == '') {this.placeholder = 'Subject*';}">
-							</div>
 
-							<div class="text-field-message">
-								<textarea name="message" id="message" title="Message" required autocomplete="off" placeholder="Message*" onfocus="if(this.placeholder == 'Message') this.placeholder='';" onblur="if(this.placeholder == '') this.placeholder='Message*';"></textarea>
-							</div>
+						<div class="text-field-name">
+							<input type="text" title="name" required autocomplete="off" name="name" id="name" placeholder="Name*" onfocus="this.placeholder = '';" onblur="if (this.placeholder == '') {this.placeholder = 'Name*';}">
+						</div>
+						<div class="text-field-name">
+							<input type="email" title="Your Email" required autocomplete="off" name="email" id="email" placeholder="Email*" onfocus="this.placeholder = '';" onblur="if (this.placeholder == '') {this.placeholder = 'Email*';}">
+						</div>
+						<div class="text-field-subject">
+							<input type="text" title="Subject" required autocomplete="off" name="subject" id="subject" placeholder="Subject*" onfocus="this.placeholder = '';" onblur="if (this.placeholder == '') {this.placeholder = 'Subject*';}">
+						</div>
 
-							<div class="text-field-submit">
-							<button class="submit-but has-spinner" onclick="send_mail();"  data-style="zoom-in" id="contact-form-submit"><span class="ladda-label">Submit</span></button>
-							</div>
-					
+						<div class="text-field-message">
+							<textarea name="message" id="message" title="Message" required autocomplete="off" placeholder="Message*" onfocus="if(this.placeholder == 'Message') this.placeholder='';" onblur="if(this.placeholder == '') this.placeholder='Message*';"></textarea>
+						</div>
+
+						<div class="text-field-submit">
+							<button class="submit-but has-spinner" onclick="send_mail();" data-style="zoom-in" id="contact-form-submit"><span class="ladda-label">Submit</span></button>
+						</div>
+
 					</div>
 				</div>
 			</div>
@@ -89,10 +88,10 @@
 	</div>
 </div>
 <script type="text/javascript">
-	$(".google-maps-div-wrap").click(function(){
+	$(".google-maps-div-wrap").click(function() {
 		$(".google-maps-div-wrap .overlay").fadeOut();
 	});
-	$(".google-maps-div-wrap").mouseleave(function(){
+	$(".google-maps-div-wrap").mouseleave(function() {
 		$(".google-maps-div-wrap .overlay").fadeIn();
 	});
 </script>
